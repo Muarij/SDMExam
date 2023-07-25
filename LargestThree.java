ee46351789421da8bba80074b1b2d8c1ccef8b51
Muarij <muarijulislammasoodi@gmail.com>
2023-07-25 09:24:42 +0530

    Update LargestThree.java
    Added Second logic:

--- LargestThree.java
+++ LargestThree.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 class LargestThree {
 
+    public static double average(int a, int b, int c) {
+        return a+b+c/3;
+
+    }
+
     public static void main (String [] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter 3 numbers to calculate its sum");
@@ -13,6 +18,9 @@ class LargestThree {
         int thirdNo=sc.nextInt();
         int sum= firstNo+ secondNo+thirdNo;
         System.out.println("The sum of three numbers is:" + sum);
+        //average
+        System.out.println("The average of three numbers is:" + average(firstNo,secondNo,thirdNo));
+
         sc.close();
     }
 }
